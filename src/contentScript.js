63ee2fad20d92e7baee2ae1567ee018e8f3e0b32
Vyacheslav.Sviridov <atx@inbox.ru>
2020-12-25 13:36:23 +0600

    initial commit

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,15 @@
-console.log("contentScript.js");
+
+      // "matches": ["https://*/login"],
+
+let loginUsername = localStorage.getItem('enmUsername')
+let loginPassword = localStorage.getItem('enmPassword')
+
+if (loginUsername === null || loginPassword === null) {
+    let promptUsername = prompt("Please enter your login name", "Harry Potter")
+    let promptPassword = prompt("Please enter your password", "Harry@Potter")
+    localStorage.setItem('enmUsername', promptUsername)
+    localStorage.setItem('enmPassword', promptPassword)
+}
+
+document.getElementById("loginUsername").value = localStorage.getItem('enmUsername')
+document.getElementById("loginPassword").value = localStorage.getItem('enmPassword')
